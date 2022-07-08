99dfd2b79b9394fbb8703c33d06c7aa113f0b29d
Jackbaklava <mhqadir14@gmail.com>
2022-07-08 21:57:31 +0500

    Worked on models.py

--- website/models.py
+++ website/models.py
@@ -1,33 +1,39 @@
+from sqlalchemy.sql import func
 from . import db
 
 
-""""
+
+# Each column has foreign keys that refer to other tables
+room_participants = db.Table("room_participants",
+    db.Column("room_id", db.Integer, db.ForeignKey("room.id")),
+    db.Column("participant_id", db.Integer, db.ForeignKey("user.id"))
+)
+
+
+"""
 relationship and backref -> placed on parent (one)
 foreign key -> placed on children (to many)
 """
-
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String())
-    password_hash = db.Column(db.String())
-    # backref allows things like message.author
-    messages = db.relationship("Message", backref="author")
-    rooms_owned = db.relationship("Room", backref="host")
-    rooms_joined = one to many
+    username = db.Column(db.String(10))
+    password_hash = db.Column(db.String(128))
+    messages = db.relationship("Message", backref="author") # message.author
+    rooms_owned = db.relationship("Room", backref="host") # message.host
+    rooms_joined = db.relationship("Room", secondary=room_participants, backref="participants") # room.participants
 
 
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String())
-    timesince = db.Column()
-    author_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    body = db.Column(db.Text(1024))
+    posted = db.Column(db.DateTime(timezone=True), default=func.now())
+    author_id = db.Column(db.Integer, db.ForeignKey("user.id")) 
     room_id = db.Column(db.Integer, db.ForeignKey("room.id"))
     
 
 class Room(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String())
-    description = db.Column(db.String())
+    name = db.Column(db.String(64))
+    description = db.Column(db.String(512))
     host_id = db.Column(db.Integer, db.ForeignKey("user.id"))
-    participants = one to many
-    messages = db.relationship("Message", backref="room")
+    messages = db.relationship("Message", backref="room") # message.room
